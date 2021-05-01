a56daa9a0ca5f0bf3cfd420c5d61b65c8ead6fd9
Matúš Baran <xbaranm@stuba.sk>
2021-05-01 14:52:50 +0200

    added logging files

---
+++ src/sk/stu/fiit/logging/Log.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.logging;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author matba
+ */
+@XmlRootElement(name = "log")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Log {
+
+    private Date date;
+    private LogLevel level;
+
+    private String classpath;
+    private String message;
+
+    public enum LogLevel {
+        FINE, INFO, WARNING, SEVERE
+    }
+
+    public Log() {
+    }
+
+    public Log(LogLevel level, String classpath, String message) {
+        this.date = new Date();
+        this.level = level;
+        this.classpath = classpath;
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate() {
+        this.date = new Date();
+    }
+
+    public LogLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(LogLevel level) {
+        this.level = level;
+    }
+
+    public String getClasspath() {
+        return classpath;
+    }
+
+    public void setClasspath(String cclass) {
+        this.classpath = cclass;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ src/sk/stu/fiit/logging/Logs.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.logging;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author matba
+ */
+@XmlRootElement(name = "logs")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Logs {
+
+    @XmlElement(name = "log")
+    List<Log> logs = null;
+
+    public List<Log> getLogs() {
+        return logs;
+    }
+
+    public void setLogs(List<Log> logs) {
+        this.logs = logs;
+    }
+
+    public static void log(Log.LogLevel level, String message, String classpath) {
+        //parse logs.xml file to Logs object
+        Logs logs = unmarshall();
+        //create new Log
+        Log newLog = new Log(level, classpath, message);
+
+        //add new Log to existing logs
+        logs.getLogs().add(newLog);
+        //write logs to logs.xml file
+        marshall(logs);
+    }
+
+    private static void marshall(Logs logs) {
+
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Logs.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            //Marshal the employees list in console
+//            jaxbMarshaller.marshal(logsObj, System.out);
+//Marshal the employees list in file
+            jaxbMarshaller.marshal(logs, new File(Paths.get("logs.xml").toString()));
+        } catch (JAXBException ex) {
+            Logger.getLogger(Logs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private static Logs unmarshall() {
+        Logs logs = new Logs();
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Logs.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            logs = (Logs) jaxbUnmarshaller.unmarshal(new File(Paths.get("logs.xml").toString()));
+
+//            for (Log log : logs.getLogs()) {
+//                System.out.println(log.getDate());
+//                System.out.println(log.getMessage());
+//                System.out.println(log.getClass());
+//            }
+        } catch (JAXBException ex) {
+            Logger.getLogger(Logs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        return logs;
+    }
+
+    public static void main(String[] args) {
+
+        Logs.marshall(
+                Logs.unmarshall()
+        );
+    }
+}
